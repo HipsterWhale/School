df75af5f136b66acee708460556d61900501fe1c
Brock Bushnell <38760132+HipsterWhale@users.noreply.github.com>
2025-04-30 12:35:32 -0600

    Completed Program3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the number guessing game.\nGuess a number between 0 and 100. ");
+
+        string response = "yes";
+        while (response.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(0, 101);
+            int guess = number + 1;
+            int guess_counter = 0;
+
+            while (number != guess)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guess_counter += 1;
+
+                if (guess > number)
+                {Console.WriteLine("Lower");}
+
+                if (guess < number)
+                {Console.WriteLine("Higher");}
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guess_counter} guesses.");
+            Console.Write("Would you like to play again? ");
+            response = Console.ReadLine();
+        }
+        Console.Write("Ok. Goodbye.");
     }
-}
+    }
