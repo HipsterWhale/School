d1727d246632215c4b4d6b0171438a1f066c5d49
Brock Bushnell <38760132+HipsterWhale@users.noreply.github.com>
2025-05-01 17:09:47 -0600

    Completed core requirements for c# prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input = 1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (input != 0)
+        {
+            Console.Write("Enter a number. ");
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+        }
+        int sum = numbers.Sum();
+        Console.WriteLine("The sum is: " + sum);
+        double mean = numbers.Average();
+        Console.WriteLine("The mean is " + mean);
+        int max = numbers.Max();
+        Console.WriteLine("The max is " + max);
     }
 }
