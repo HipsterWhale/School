6d46b650983b31b764e59d3e3058c6a3e0eb9115
Brock Bushnell <38760132+HipsterWhale@users.noreply.github.com>
2025-05-06 14:01:23 -0600

    Completed W03 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,23 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job2._jobTitle = "Mechatronics Engineer";
+        job1._company = "Alta Trust Company";
+        job2._company = "Zooks";
+        job1._startYear = 2025;
+        job2._startYear = 2019;
+        job1._endYear = 2025;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Peter Piper";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,36 @@
 using System;
 
+class Circle
+{
+    private double _radius;
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+    public double GetRadius()
+    {
+        return _radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius():f2}");
+
+        Circle myCircle2 = new Circle();
+        myCircle2.SetRadius(20);
+
+
+
     }
 }
